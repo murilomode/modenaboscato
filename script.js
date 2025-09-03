1f46f7c1686523715b5328871063f33c11768ea2
murilomode <murilomodena121@gmail.com>
2025-09-03 16:19:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.getElementById('magic-button').addEventListener('click', function() {
+    // Esconde o botão após o clique
+    document.getElementById('magic-button').style.display = 'none';
+    
+    // Exibe a mensagem de progresso
     document.getElementById('status-message').classList.remove('hidden');
+
+    // Após 3 segundos, exibe a mensagem final e esconde a de progresso
     setTimeout(function() {
         document.getElementById('status-message').classList.add('hidden');
         document.getElementById('final-message').classList.remove('hidden');
-    }, 3000); // Espera 3 segundos antes de exibir a mensagem final
+    }, 3000);
 });
+
